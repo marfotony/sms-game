0fe8efe3145c6b9c276d7dac5947830b2d741451
Callum Powell <callumpowell@outlook.com>
2018-03-15 22:05:32 +0100

    added env variable to allow other mongodb hosts than localhost

--- _config.js
+++ _config.js
@@ -1,10 +1,11 @@
 
 var config = {};
 
+var mongoHost = process.env.MONGO_HOST || 'localhost'
 config.mongoURI = {
-	production: 'mongodb://localhost/sms-game',
-	development: 'mongodb://localhost/sms-game-dev',
-	test: 'mongodb://localhost/sms-game-test'
+	production: 'mongodb://' + mongoHost + '/sms-game',
+	development: 'mongodb://' + mongoHost + '/sms-game-dev',
+	test: 'mongodb://' + mongoHost + '/sms-game-test'
 };
 
 module.exports = config;

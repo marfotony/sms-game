2237dd0f16e20e304bf01620ffabfd6033152fc0
Callum Powell <callumpowell@outlook.com>
2018-03-15 20:48:02 +0100

    remove hardcoded username/password and added reference to environment variables

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const port = process.env.PORT || 3000;
 
 var db = require('./db');
 
+if (!process.env.TWILIO_USER) console.log('WARNING: Environment variable TWILIO_USER not set!');
+if (!process.env.TWILIO_PW) console.log('WARNING: Environment variable TWILIO_PW not set!');
+if (!process.env.ADMIN_USER) console.log('WARNING: Environment variable ADMIN_USER not set!');
+if (!process.env.ADMIN_PW) console.log('WARNING: Environment variable ADMIN_PW not set!');
 
 var login = basic(function verify(challenge, callback) {
-	if (challenge.username === 'admin' && challenge.password === 'secret') {
+	if (challenge.username === process.env.TWILIO_USER && challenge.password === process.env.TWILIO_PW) {
 		callback(null, true, { user: 'twilio' });
+	} else if (challenge.username === process.env.ADMIN_USER && challenge.password === process.env.ADMIN_PW) {
+		callback(null, true, { user: 'admin' });
 	} else {
 		callback(null, false, { error: 'INVALID_PASSWORD' });
 	}
@@ -31,70 +37,3 @@ app.use('/attempts', auth.required(), AttemptsController);
 app.listen(port, () => console.log('sms-game listening on port 3000!'));
 
 module.exports = app
-
-/*
-Environment variables:
-TWILIO_USER
-TWILIO_PW
-
-ADMIN_USER
-ADMIN_PW
-
-Twilio integration must have basic auth.
-POST /users (phoneNumber, username)
-POST /attempts (phoneNumber, code)
-
-Admin page must have basic auth.
-GET /users
-GET /users/:id
-POST /users
-PUT /users/:id
-DELETE /users/:id
-
-GET /codes
-GET /codes/:id
-POST /codes
-PUT /codes/:id
-DELETE /codes/:id
-
-GET /attempts
-GET /attempts/:id
-POST /attempts
-PUT /attempts/:id
-DELETE /attempts/:id
-
-user: {
-	"__v": 0,
-    "_id": "5aa4301c6c935cbc0287022b",
-    "phoneNumber": "+4792037340",
-    "username": "callum0x50"
-}
-
-attempt: {
-	phoneNumber: String,
-	createdAt: Date,
-	code: String
-}
-
-code: {
-	code: String,
-	createdAt: Date,
-	activatesAt: Date,
-	expiresAt: Date
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

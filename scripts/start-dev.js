aa3c6946d12bd315f2f18acdc8b524ceca27db7c
Callum Powell <callumpowell@outlook.com>
2018-03-15 21:31:57 +0100

    configured npm scripts for dev and test environments

---
+++ scripts/start-dev.js
@@ -0,0 +1,10 @@
+
+process.env.NODE_ENV = 'development'
+
+process.env.TWILIO_USER = 'twilio'
+process.env.TWILIO_PW = 'secret'
+
+process.env.ADMIN_USER = 'admin'
+process.env.ADMIN_PW = 'secret'
+
+var server = require('../server.js');
